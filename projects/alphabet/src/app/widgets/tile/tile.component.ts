8e60a0044873721193185201a76dd6b0faef619d
aaron-plahn <aplahn85@gmail.com>
2021-02-08 17:29:40 -0800

    feat(alphabet/tile): add scrolling logic to tile

--- projects/alphabet/src/app/widgets/tile/tile.component.ts
+++ projects/alphabet/src/app/widgets/tile/tile.component.ts
@@ -8,7 +8,7 @@ import { Card } from '../../services/IAlphabetAPI';
   styleUrls: ['./tile.component.css']
 })
 export class TileComponent implements OnInit {
-
+  alphabetSize: number;
   card: Card;
   _tileNumber: number;
 
@@ -19,9 +19,42 @@ export class TileComponent implements OnInit {
   constructor( private data: AlphabetService ) { }
 
   ngOnInit(): void {
+    this.data.getAlphabetSize().subscribe((data:number)=>{
+      this.alphabetSize = data;
+    }); // Switch to pipe map
+    this.updateCard(this._tileNumber);
+  }
+
+  updateCard(newTileNumber: number){
+    if(newTileNumber < 0 || !Number.isInteger(newTileNumber)) throw new Error(`Tile number must be a positive integer.`);
+    this._tileNumber = newTileNumber;
     this.data.getCardBySequenceNumber(this._tileNumber).subscribe((data:Card)=>{
       this.card = data;
     });
+  }
+
+  cyclicIncrement(n:number,maxNumber:number){
+    n++;
+    if(n < 0 || n > maxNumber + 1) throw new Error(`Index ${n} out of bounds.`); 
+    return (n++) % maxNumber;
+  }
+
+  cyclicDecrement(n:number,maxNumber:number){
+    let startingIndex: number = 1;
+    n--;
+    if(n === startingIndex - 1) return maxNumber;
+    if(n < startingIndex || n > maxNumber) throw new Error(`Index out of bounds: ${n}`);
+    return n;
+  }
+
+  handleLeftArrowClick(){
+    this._tileNumber = this.cyclicDecrement(this._tileNumber,this.alphabetSize);
+    this.updateCard(this._tileNumber);
+  }
+
+  handleRightArrowClick(){
+    this._tileNumber = this.cyclicIncrement(this._tileNumber,this.alphabetSize);
+    this.updateCard(this._tileNumber);
   }
 
 }
